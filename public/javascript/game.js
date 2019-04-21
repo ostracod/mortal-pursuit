fc5d3038690ec5f0517b82c3125e244c3da94a79
John Eisenmann <esperantanaso@gmail.com>
2019-04-21 15:42:34 -0400

    Player walking

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -7,6 +7,8 @@ var spritesImageSize = 8;
 var canvasPixelWidth;
 var canvasPixelHeight
 var frameNumber = 0;
+var playerList = [];
+var localPlayer;
 
 var tileData = [
     "...............",
@@ -44,8 +46,8 @@ function drawSprite(pos, which) {
         tempClipY,
         spriteSize,
         spriteSize,
-        pos.x * spriteScale,
-        pos.y * spriteScale,
+        Math.round(pos.x) * spriteScale,
+        Math.round(pos.y) * spriteScale,
         spriteSize * spriteScale,
         spriteSize * spriteScale
     );
@@ -65,6 +67,69 @@ function getTile(pos) {
     return tempLine.charCodeAt(Math.floor(pos.x / spriteSize));
 }
 
+function Player(pos, color) {
+    this.pos = pos;
+    this.color = color;
+    this.direction = 1;
+    this.isWalking = false;
+    this.isDucking = false;
+    this.walkFrameCount = 0;
+    this.isBlinking = false;
+    this.blinkDelay = 0;
+    this.maximumBlinkDelay = 20;
+    playerList.push(this);
+}
+
+Player.prototype.startWalking = function(direction) {
+    if (localPlayer.direction == direction && localPlayer.isWalking) {
+        return;
+    }
+    if (!localPlayer.isWalking) {
+        this.walkFrameCount = 0;
+    }
+    this.direction = direction;
+    this.isWalking = true;
+}
+
+Player.prototype.stopWalking = function(direction) {
+    if (localPlayer.direction != direction || !localPlayer.isWalking) {
+        return;
+    }
+    this.isWalking = false;
+}
+
+Player.prototype.tick = function() {
+    if (this.isWalking && !this.isDucking) {
+        this.pos.x += this.direction;
+        this.walkFrameCount += 1;
+    }
+    this.isBlinking = (this.blinkDelay < 4);
+    this.blinkDelay += 1;
+    if (this.blinkDelay > this.maximumBlinkDelay) {
+        this.blinkDelay = 0;
+        this.maximumBlinkDelay = 80 + Math.floor(Math.random() * 200);
+    }
+}
+
+Player.prototype.draw = function() {
+    var tempSprite;
+    if (this.isDucking) {
+        tempSprite = 3;
+    } else if (this.isWalking && this.walkFrameCount % 12 < 6) {
+        tempSprite = 1;
+    } else {
+        tempSprite = 0;
+    }
+    if (this.direction < 0) {
+        tempSprite += 4;
+    }
+    if (this.isBlinking) {
+        tempSprite += 8;
+    }
+    tempSprite += this.color * 16;
+    drawSprite(this.pos, tempSprite);
+}
+
 function ClientDelegate() {
     
 }
@@ -72,6 +137,7 @@ function ClientDelegate() {
 ClientDelegate.prototype.initialize = function() {
     canvasPixelWidth = Math.floor(canvasWidth / spriteScale);
     canvasPixelHeight = Math.floor(canvasHeight / spriteScale);
+    localPlayer = new Player(new Pos(spriteSize * 3, spriteSize * 6), 0);
     initializeSpriteSheet(function() {});
 }
 
@@ -84,6 +150,12 @@ ClientDelegate.prototype.addCommandsBeforeUpdateRequest = function() {
 }
 
 ClientDelegate.prototype.timerEvent = function() {
+    var index = 0;
+    while (index < playerList.length) {
+        var tempPlayer = playerList[index];
+        tempPlayer.tick();
+        index += 1;
+    }
     clearCanvas();
     var tempPos = new Pos(0, 0);
     while (tempPos.y < canvasPixelHeight) {
@@ -91,7 +163,7 @@ ClientDelegate.prototype.timerEvent = function() {
         var tempSprite = null;
         // Enemy.
         if (tempTile == 88) {
-            if (frameNumber % 16 < 8) {
+            if (frameNumber % 20 < 10) {
                 tempSprite = 40;
             } else {
                 tempSprite = 41;
@@ -112,6 +184,12 @@ ClientDelegate.prototype.timerEvent = function() {
             tempPos.y += spriteSize;
         }
     }
+    var index = 0;
+    while (index < playerList.length) {
+        var tempPlayer = playerList[index];
+        tempPlayer.draw();
+        index += 1;
+    }
     frameNumber += 1;
 }
 
@@ -119,12 +197,28 @@ ClientDelegate.prototype.keyDownEvent = function(keyCode) {
     if (focusedTextInput !== null) {
         return true;
     }
-    
+    if (keyCode == 65 || keyCode == 37) {
+        localPlayer.startWalking(-1);
+    }
+    if (keyCode == 68 || keyCode == 39) {
+        localPlayer.startWalking(1);
+    }
+    if (keyCode == 83 || keyCode == 40) {
+        localPlayer.isDucking = true;
+    }
     return true;
 }
 
 ClientDelegate.prototype.keyUpEvent = function(keyCode) {
-    
+    if (keyCode == 65 || keyCode == 37) {
+        localPlayer.stopWalking(-1);
+    }
+    if (keyCode == 68 || keyCode == 39) {
+        localPlayer.stopWalking(1);
+    }
+    if (keyCode == 83 || keyCode == 40) {
+        localPlayer.isDucking = false;
+    }
     return true;
 }
 
