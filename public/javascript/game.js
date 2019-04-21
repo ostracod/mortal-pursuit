15c7d0a47e810958cf136221f34b11f30d925e88
John Eisenmann <esperantanaso@gmail.com>
2019-04-21 12:10:48 -0400

    Drawing the world

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -4,6 +4,32 @@ var spriteScale = 5;
 var spritesImageHasLoaded = false;
 var spritesImage;
 var spritesImageSize = 8;
+var canvasPixelWidth;
+var canvasPixelHeight
+var frameNumber = 0;
+
+var tileData = [
+    "...............",
+    "...............",
+    "...............",
+    "...............",
+    ".#...........#.",
+    ".#.......X...#.",
+    ".1...........2.",
+    "====].[========",
+    "OOOOOXOOOOOOOOO",
+    "OOOOOOOOOOOOOOO",
+];
+
+var tileSpriteMap = {
+    49: 40, // Goal 1.
+    50: 41, // Goal 2.
+    35: 52, // Wall.
+    91: 48, // Left grass.
+    61: 49, // Center grass.
+    93: 50, // Right grass.
+    79: 51 // Ground.
+};
 
 function drawSprite(pos, which) {
     if (!spritesImageHasLoaded) {
@@ -34,11 +60,18 @@ function initializeSpriteSheet(done) {
     spritesImage.src = "/images/sprites.png";
 }
 
+function getTile(pos) {
+    var tempLine = tileData[Math.floor(pos.y / spriteSize)];
+    return tempLine.charCodeAt(Math.floor(pos.x / spriteSize));
+}
+
 function ClientDelegate() {
     
 }
 
 ClientDelegate.prototype.initialize = function() {
+    canvasPixelWidth = Math.floor(canvasWidth / spriteScale);
+    canvasPixelHeight = Math.floor(canvasHeight / spriteScale);
     initializeSpriteSheet(function() {});
 }
 
@@ -52,16 +85,34 @@ ClientDelegate.prototype.addCommandsBeforeUpdateRequest = function() {
 
 ClientDelegate.prototype.timerEvent = function() {
     clearCanvas();
-    var tempSprite = 0;
-    var tempPos = new Pos(0, spriteSize);
-    while (tempPos.x < canvasWidth) {
-        drawSprite(tempPos, tempSprite);
+    var tempPos = new Pos(0, 0);
+    while (tempPos.y < canvasPixelHeight) {
+        var tempTile = getTile(tempPos);
+        var tempSprite = null;
+        // Enemy.
+        if (tempTile == 88) {
+            if (frameNumber % 16 < 8) {
+                tempSprite = 32;
+            } else {
+                tempSprite = 33;
+            }
+        } else if (tempTile in tileSpriteMap) {
+            tempSprite = tileSpriteMap[tempTile];
+        }
+        if (tempSprite !== null) {
+            drawSprite(tempPos, tempSprite);
+        }
         tempSprite += 1;
         if (tempSprite > 5) {
             tempSprite = 0;
         }
         tempPos.x += spriteSize;
+        if (tempPos.x >= canvasPixelWidth) {
+            tempPos.x = 0;
+            tempPos.y += spriteSize;
+        }
     }
+    frameNumber += 1;
 }
 
 ClientDelegate.prototype.keyDownEvent = function(keyCode) {
